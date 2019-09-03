f9fc665419f2072deab2b5f38e9adaabc3162d52
Ljubisa Sehovac <sehovaclj@gamil.com>
2019-09-03 13:20:54 -0400

    Starting CNN code: importing functions

--- fruit_class1.py
+++ fruit_class1.py
@@ -1,7 +1,11 @@
-# for self learning, from square 0 (full image) to full image classification model
+# python code that:
+# 	loads the training and testing pickle files from 
+#	splits the test set into a validation set and final test set
+#	contains CNN model to be used for fruit classification
+#	training, validation, and update of parameters
 
-# over 65k total images, 95 different fruits
 
+# importing functions
 
 import numpy as np
 import pandas as pd
@@ -26,64 +30,4 @@ import os
 
 
 ############################################################################################
-
-# loop through all files. loop through all images. read images --> get pixels --> store pixels
-# as dict --> append dict to massive list
-
-# FOR TRIANING
-
-all_fruit_files_train = []
-
-for fruit_file in glob.glob("Training/*"):
-	all_fruit_files_train.append(fruit_file)
-
-
-# store pixel values as dict
-
-IMAGES_TRAINING = []
-
-for i in range(len(all_fruit_files_train)):
-	images_file = glob.glob(all_fruit_files_train[i] + '/*.jpg')
-	for j in images_file:
-		im = Image.open(j)
-		w, h = im.size
-		pixels = list(im.getdata())
-		pixels = np.array(pixels).reshape(w, h, 3)
-		dict = { i : pixels }
-		IMAGES_TRAINING.append(dict)
-
-
-
-
-# FOR TESTING
-
-
-IMAGES_TESTING = []
-
-
-all_fruit_files_test = []
-
-for fruit_file in glob.glob("Test/*"):
-	all_fruit_files_test.append(fruit_file)
-
-
-# store pixel values as dict
-
-for i in range(len(all_fruit_files_test)):
-	images_files = glob.glob(all_fruit_files_test[i] + '/*.jpg')
-	for j in images_file:
-	im = Image.open(j)
-	w, h = im.size
-	pixels = list(im.getdata())
-	pixels = np.array(pixels).reshape(w, h, 3)
-	dict = { i : pixels }
-	IMAGES_TESTING.append(dict)
-
-
-
-
-
-
-
-
 
